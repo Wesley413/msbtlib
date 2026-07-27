02fb26d64bf0650f42f9dcd37a24d2b2dbbccdf6
Wesley413 <weslewgamer@gmail.com>
2026-07-27 15:28:30 -0300

    chore: remove remaining legacy code

--- src/msbtlib/msbt.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-from pathlib import Path
-import struct
-from io import BufferedReader, BytesIO
-from .classes import MsbtHeader, MsbtLbl1, MsbtAtr1, MsbtTxt2, MsbtAto1, MsbtTsy1, Text, Command
-from .utils import align_block_skip, skip
-from typing import Self
-import json
-from pprint import pprint
--- src/msbtlib/msbt_writer.py
+++
@@ -1,6 +0,0 @@
-from .msbt import Msbt
-from io import BytesIO
-from typing import Literal
-import struct
-from .classes import *
-import json
